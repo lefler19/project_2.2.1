306f188f21b70c4ae888f49ec78834c3cd23b8d6
Настя <west_2017@mail.ru>
2023-09-26 21:51:55 +0300

    2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,10 +1,12 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -14,21 +16,32 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      User user1 = new User("Джордж", "Вашингтон", "vashington@gmail.com");
+      User user2 = new User("Джо", "Байден", "bayden@gmail.com");
+      User user3 = new User("Дональд", "Трамп", "tramp@gmail.com");
+      User user4 = new User("Барак", "Обама", "obama@gmail.com");
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car1 = new Car("Lincoln", 999);
+      Car car2 = new Car("Dodge", 777);
+      Car car3 = new Car("Ford", 666);
+      Car car4 = new Car("Cadillac", 111);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3.setCar(car3).setUser(user3));
+      userService.add(user4.setCar(car4).setUser(user4));
+
+
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
       }
+      System.out.println(userService.getCarInfo("Lincoln", 999));
 
+      try {
+         userService.getCarInfo("Jeep", 555);
+      } catch (NoResultException e) {
+         System.out.println("User not found");
+      }
       context.close();
    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -19,8 +19,12 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne(cascade=CascadeType.ALL, mappedBy = "user")
+   @JoinColumn(name = "car")
+   private Car car;
+
    public User() {}
-   
+
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
@@ -57,5 +61,20 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+   public Car getCar() {
+      return car;
+   }
+   public Car setCar(Car car) {
+      this.car = car;
+      return car;
+   }
+   @Override
+   public String toString() {
+      return "User {" +
+              "name = " + firstName +
+              ", Last name = " + lastName +
+              ", email = " + email +
+              '}';
    }
 }
